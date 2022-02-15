9caa79ef925b79a7a7180556c229d8d3697428fd
Simão Neto <simaomenezesneto@gmail.com>
2022-02-15 23:23:45 +0000

    :tada: challenge finalized

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,29 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(!newTaskTitle) return;
+    const newTask: Task = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false
+    }
+    setTasks(oldTask => [...oldTask, newTask]);
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTasks = tasks.map(t => t.id === id ? {
+      ...t,
+      isComplete: !t.isComplete
+    } : t);
+    setTasks(newTasks);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const taskFilter = tasks.filter(e => e.id !== id);
+    setTasks(taskFilter);
   }
 
   return (
